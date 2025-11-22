652885d9174e1c165fce6e742880b4e5740089d9
MHEien <markushei@hotmail.no>
2025-11-22 12:15:46 +0100

    chore: update package.json scripts to use bunx for command execution and refactor exports in editor index file

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -1,8 +1,6 @@
-import { type Config, createUsePuck, type Data, Puck } from "@measured/puck";
+export { type Config, createUsePuck, type Data, Puck } from "@measured/puck";
 
 export * from "./config";
 export * from "./editor";
 export * from "./render";
 export * from "./types";
-
-export { createUsePuck, Puck, type Config, type Data };
